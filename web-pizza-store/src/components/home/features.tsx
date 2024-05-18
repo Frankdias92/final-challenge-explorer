a4846e10f8629fad3bdc1c80087c055afcaa04fa
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-05-18 14:51:30 -0300

    feat: section features | created component list products

--- web-pizza-store/src/components/home/features.tsx
+++ web-pizza-store/src/components/home/features.tsx
@@ -1,30 +1,49 @@
-import { Image } from "@nextui-org/react";
-import NextImage from "next/image";
+import { ListProductsFeatures } from "./listProdutsFeatures";
+
 
 export function Features() {
-    const pathImg = 'https://raw.githubusercontent.com/Frankdias92/final-challenge-explorer/main/web-pizza-store/src/assets/menu'
+
+    const products = [
+        {
+            id: 1,
+            image: "Mask%20group-6.png",
+            title: "Salada Ravanello",
+            price: "49,97",
+            amount: "1"
+        },
+        {
+            id: 2,
+            image: "Mask%20group-1.png",
+            title: "Salada Ravanello",
+            price: "49,97",
+            amount: "1"
+        },
+        {
+            id: 3,
+            image: "Mask%20group-2.png",
+            title: "Salada Ravanello",
+            price: "49,97",
+            amount: "1"
+        },
+    ]
 
 
     return (
         <div className="flex flex-col w-full h-full justify-center font-poppins text-light-300 antialiased">
-            <h3 className="font-medium text-lg">Section Name</h3>
+            <h3 className="font-medium text-lg ">Section Name</h3>
 
-            <div className="flex flex-col w-[210px] h-[292px] rounded-lg bg-dark-900 border-0 outline-none
-            ring-1 ring-dark-800 ">
-                <div className="flex flex-col w-full h-full justify-start items-center p-6">
-                    <span className="flex size-[88px] bg-cover">
-                        <Image
-                            as={NextImage}
-                            width={88}
-                            height={88}
-                            src={`${pathImg}/Mask%20group.png`}
-                            alt="NextUI hero Image"
-                            className="flex"
+            <div className="flex w-full gap-4 overflow-hidden">
+                {products.map(item => {
+                    return (
+                        <ListProductsFeatures
+                        key={item.id}
+                        image={item.image}
+                        title="Salada Ravanello"
+                        price="49,97"
+                        amount="1"
                         />
-                    </span>
-
-                    <span>test</span>
-                </div>
+                    )
+                })}
             </div>
         </div>
     )
---
+++ web-pizza-store/src/components/home/listProdutsFeatures.tsx
@@ -0,0 +1,48 @@
+import { Image } from "@nextui-org/react";
+import NextImage from "next/image";
+import { ButtonText } from "../buttonText";
+import { GoDash, GoPlus } from "react-icons/go";
+
+
+interface ListProps {
+    image: string 
+    title: string 
+    price: string 
+    amount: string
+}
+
+export function ListProductsFeatures({ image, title, price, amount }: ListProps) {
+    const pathImg = 'https://raw.githubusercontent.com/Frankdias92/final-challenge-explorer/main/web-pizza-store/src/assets/menu'
+
+    return (
+        <div className="flex flex-col w-[210px] h-[292px] rounded-lg bg-dark-900 border-0 outline-none
+            ring-1 ring-dark-800 ">
+                <div className="flex flex-col w-full h-full justify-start items-center gap-3 p-6">
+                    <span className="flex size-[88px] bg-cover">
+                        <Image
+                            as={NextImage}
+                            width={88}
+                            height={88}
+                            src={`${pathImg}/${image}`}
+                            alt="NextUI hero Image"
+                            className="flex"
+                        />
+                    </span>
+
+                    
+                    <div className="flex flex-col items-center w-full h-full gap-3">
+                        <h2>{title} {`>`}</h2>
+                        <span>R$ {price}</span>
+
+                        <div className="flex justify-center items-center gap-x-4 text-lg text-white">
+                            <GoDash />
+                            <span className="text-light-300">{amount}</span>
+                            <GoPlus />
+                        </div>
+
+                        <ButtonText text="incluir"/>
+                    </div>
+                </div>
+            </div>
+    )
+}
