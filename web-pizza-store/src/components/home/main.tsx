21baae0df1d35e8b5f31816d6805c990cee18ed3
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-05-18 10:29:53 -0300

    doc: config domain next image and upload a new image in assets

--- web-pizza-store/src/components/home/main.tsx
+++ web-pizza-store/src/components/home/main.tsx
@@ -1,10 +1,17 @@
-
+import { Image } from "@nextui-org/react";
+import NextImage from "next/image";
 
 export function Main() {
 
     return (
         <main className="bg-slate-500 flex w-full min-h-32 px-7"> 
-            text
+            <Image
+                as={NextImage}
+                width={300}
+                height={200}
+                src="https://raw.githubusercontent.com/Frankdias92/final-challenge-explorer/main/web-pizza-store/src/assets/menu/Mask%20group-1.png"
+                alt="NextUI hero Image"
+            />
         </main>
     )
 }
