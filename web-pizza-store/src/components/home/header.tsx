149d6bf20b22ec73cf0500568693cd79338f406b
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-05-19 10:25:04 -0300

    feat: header menu overlay | working on overlay

--- web-pizza-store/src/components/home/header.tsx
+++ web-pizza-store/src/components/home/header.tsx
@@ -11,15 +11,24 @@ export function Header() {
     const [menuIsOpen, setMenuOpen] = useState(false)
     
     return (
-        <header className="grid w-full h-28 px-7 pb-8 bg-dark-400 content-end text-white">
+        <header className="grid w-full h-28 px-7 pb-8 bg-dark-400 content-end text-white relative">
             {menuIsOpen ? (
-            <button
-                className="flex w-full h-8 items-center gap-4 text-2xl content-end"
-                onClick={() => setMenuOpen(false)}
-            >
-                <CgClose/>
-                Menu 
-            </button>
+                <div className="w-full ">
+                    <button
+                        className="flex w-full h-8 items-center gap-4 text-2xl content-end"
+                        onClick={() => setMenuOpen(false)}
+                    >
+                        <CgClose/>
+                        Menu 
+                    </button>
+
+                    <div className="absolute flex flex-col w-full min-h-screen overflow-auto bg-red-800 top-28 left-0 z-30">
+                        <div className="flex flex-col w-full h-full">
+                            <span>23</span>
+                            <span>64</span>
+                        </div>
+                    </div>
+                </div>
             ) : (
                 <div className="flex w-full content-end justify-between items-center">
                     <button
@@ -39,10 +48,11 @@ export function Header() {
                         <span className="flex w-6 h-6 p-2 justify-center items-center rounded-full bg-tint-tomato-400 absolute
                         top-0 right-0 translate-x-1/2 -translate-y-1/4">
                             <span className="flex items-center justify-center font-medium text-sm">
-                                5
+                                5   
                             </span>
                         </span>
                     </span>
+
                 </div>
                 
             )}
