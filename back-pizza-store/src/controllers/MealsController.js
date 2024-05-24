a4b32574791e6efa0d07f77ad162e0c0a96c8aae
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-05-24 09:16:05 -0300

    build: router meals finish | everything works

--- back-pizza-store/src/controllers/MealsController.js
+++ back-pizza-store/src/controllers/MealsController.js
@@ -6,7 +6,10 @@ const AppError = require("../utils/AppError");
 class MealsController {
     async create(req, res) {
         const { name, description, price, category, created_by } = req.body
-        const [ meal_id ] = await knex("meals").insert({ name, description, price, category, created_by }).returning("meal_id")
+        
+        const [ meal_id ] = await knex("meals")
+        .insert({ name, description, price, category, created_by })
+        .returning("meal_id")
 
         res.status(201).json({ meal_id })
     }
@@ -16,6 +19,40 @@ class MealsController {
         res.status(200).json(meals)
     }
 
+    async item(req, res) {
+        const meal = await knex("meals").where({ meal_id: req.params.id })
+
+        if (meal) {
+            res.status(200).json(meal)
+        } else {
+            res.status(404).json({ error: "Meal not found" })
+        }
+    }
+
+    async update(req, res) {
+        const { name, description, price, category, created_by } = req.body
+
+        const meal = await knex("meals").where({ meal_id: req.params.id })
+        .update({
+            name,
+            description, price,
+            category,
+            created_by
+        })
+
+        res.status(200).json({ message: "Meal update" })
+    }
+
+    async delete(req, res) {
+        const meal = await knex("meals").where({ meal_id: req.params.id })
+        .delete()
+
+        if (meal) {
+            res.status(200).json({ message: "Meal deleted" })
+        } else {
+            res.status(404).json({ error: "Meal not found" })
+        }
+    }
 
 }
 
--- back-pizza-store/src/routes/meals.routes.js
+++ back-pizza-store/src/routes/meals.routes.js
@@ -6,6 +6,9 @@ const mealsRoutes = Router()
 const mealsController = new MealsController()
 
 mealsRoutes.get('/', mealsController.index)
+mealsRoutes.get('/:id', mealsController.item)
 mealsRoutes.post('/', mealsController.create)
+mealsRoutes.put('/:id', mealsController.update)
+mealsRoutes.delete('/:id', mealsController.delete)
 
 module.exports = mealsRoutes
