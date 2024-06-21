e811ac869134ead4c9b51cae198e66d7976af41d
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-06-21 11:31:24 -0300

    :ok_hand: style: adjust consistent font style from figma

--- web-pizza-store/src/components/home/footer.tsx
+++ web-pizza-store/src/components/home/footer.tsx
@@ -10,12 +10,14 @@ export function Footer() {
         >
 
             <div className="flex w-full md:w-3/4 md:items-center justify-between md:m-auto">
-                <span className="flex w-fit items-start md:justify-center font-bold gap-2 text-base text-light-700">
+                <span className="flex w-fit items-start md:justify-center font-bold gap-2 text-base text-light-700
+                font-roboto lg:text-2xl antialiased"
+            >
                     <span className="text-tint-cake-400 text-2xl"><BsFillHexagonFill /></span>
                     food explorer
                 </span>
 
-                <p className="flex w-fit flex-col items-end text-light-200 text-xs font-dmsams gap-2 md:flex-row">
+                <p className="flex w-fit flex-col items-end text-light-200 text-xs lg:text-sm font-dmsams gap-2 md:flex-row antialiased">
                     © 2024 - Todos os direitos reservados.
 
                     <Link href={'https://www.linkedin.com/in/franklinmacedodias/'} target="_blank">
