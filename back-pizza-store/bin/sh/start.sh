f2091bea2130a140111239c31efce5560de39c6d
Frankdias92 <frankmcdias@gmail.com>
2024-08-08 16:49:29 -0300

    try to install docker script

--- back-pizza-store/bin/sh/start.sh
+++ back-pizza-store/bin/sh/start.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 echo "Iniciando o servidor..."
 
-# Iniciar o contêiner do PostgreSQL
-docker run -d --name db \
-  -e POSTGRES_USER=${POSTGRES_USER} \
-  -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-  -e POSTGRES_DB=${POSTGRES_DB} \
-  -p 5432:5432 \
-  postgres:16.2-alpine3.19
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh --dry-run
 
 # Esperar pelo PostgreSQL estar disponível
 until nc -z -v -w30 db 5432
