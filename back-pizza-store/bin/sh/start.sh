bcfdbbc9952d238fd2026baad027264a20a92be5
Frankdias92 <frankmcdias@gmail.com>
2024-08-08 16:29:37 -0300

    start postgres

--- back-pizza-store/bin/sh/start.sh
+++ back-pizza-store/bin/sh/start.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 echo "Iniciando o servidor..."
-npm start
+
+# Iniciar o contêiner do PostgreSQL
+docker run -d --name db \
+  -e POSTGRES_USER=${POSTGRES_USER} \
+  -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+  -e POSTGRES_DB=${POSTGRES_DB} \
+  -p 5432:5432 \
+  postgres:16.2-alpine3.19
+
 # Esperar pelo PostgreSQL estar disponível
 until nc -z -v -w30 db 5432
 do
@@ -9,8 +17,9 @@ do
 done
 echo "Postgres está disponível - continuando"
 
-# Executar migrações
-npm run migrate
 
-# Iniciar o servidor
+npm run migrate
+npm start
 node src/server.js
+
+tail -f /dev/null
