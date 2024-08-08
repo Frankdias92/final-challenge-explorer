20040b87914b712207d598cbbaf72885436afc50
Frankdias92 <frankmcdias@gmail.com>
2024-08-08 10:22:30 -0300

    add deploy cors origin

--- back-pizza-store/src/server.js
+++ back-pizza-store/src/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-  origin: [`${process.env.APP_CORS_ORIGIN}:${process.env.APP_CORS_PORT}`],
+  origin: [`${process.env.APP_CORS_ORIGIN}`],
   credentials: true
 }));
 
